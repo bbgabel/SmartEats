5026f79430e2d3766931efa5022c22832b295130
bbgabel <bbgabel5@gmail.com>
2023-10-10 17:14:08 -0600

    header modularization

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Start from './components/Start';
 import logo from './components/logo.jpg';
-import Hamburger from './components/Items/Hamburger';
 import About from './components/About';
+import Slider from './components/Items/Slider';
 import './App.css';
 import './components/Items/Items.css'
 
 function Home() {
 
-  const [isHamOpen, setIsHamOpen] = useState(false);
-
-  const toggleHam = () => {
-    setIsHamOpen(!isHamOpen);
-  }
-
   return (
     <div className="App">
-      <header className="App-header-real">
-        <div className="left">
-          <button className="burger-button" onClick={toggleHam}>
-        <Hamburger />
-        </button>
-        <div className={`sliding-menu ${isHamOpen ? 'open' : ''}`}>
-          <button className="burger-button-modal" onClick={toggleHam}>
-          <Hamburger />
-          </button>
-          <Link to="/" className="Menu-buttons button2">
-          <i className="fas fa-house fa-xs"> Home</i>
-          </Link>
-          <p />
-          <Link to="/About" className="Menu-buttons button1">
-          <i className="fas fa-users fa-xs"> About Us</i>
-          </Link>
-        </div>
-        </div>
-        <div className="center">
-        Welcome to SmartEatz!
-        </div>
-      </header>
+        <Slider />
       <header className="App-header">
         <h3>Click the logo to started!</h3>
         <Link to="/Start" className="App-button">
--- src/components/Items/Slider.js
+++ src/components/Items/Slider.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Hamburger from './Hamburger';
 
 export default function Slider() {
+
+    const [isHamOpen, setIsHamOpen] = useState(false);
+
+    const toggleHam = () => {
+    setIsHamOpen(!isHamOpen);
+  }
+
     return (
         <div>
-            <h1>About</h1>
+            
+            <header className="Slider">
+                <button className="burger-button" onClick={toggleHam}>
+                    <div className="left">
+                        <Hamburger />
+                    </div>
+                </button>
+            <div className="center">
+            Welcome to SmartEatz!
+            </div>
+            </header>
+            <div className={`sliding-menu ${isHamOpen ? 'open' : ''}`}>
+            <button className="burger-button-modal" onClick={toggleHam}>
+            <Hamburger />
+            </button>
+            <Link to="/" className="Menu-buttons button2">
+            <i className="fas fa-house fa-xs"> Home</i>
+            </Link>
+            <p />
+            <Link to="/About" className="Menu-buttons button1">
+            <i className="fas fa-users fa-xs"> About Us</i>
+            </Link>
+            </div>
+
         </div>
     )
 }
