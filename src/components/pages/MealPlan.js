e5b75626ece0004ac2180622c6b27b959ca1bd47
bbgabel <bbgabel5@gmail.com>
2023-12-05 06:31:50 -0700

    Big changes

--- src/components/pages/MealPlan.js
+++ src/components/pages/MealPlan.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from 'react';
+import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Slider from '../Items/Slider';
@@ -48,31 +48,99 @@ export default function MealPlan() {
         body,
       }
 
+      const [mealData, setMealData] = useState(null);
+
       const sendApiRequest = () => {
         axios.post('http://localhost:3000/api', requestData)
         .then(response => {
             const mealData = response.data;
-            console.log(mealData);
-            console.log(mealData.breakfast);
-            console.log(mealData.breakfast.Calories);
+            setMealData(mealData);
+            console.log(mealData.breakfast.Names);
           })
           .catch(error => {
             console.error("API Request Error:", error);
           })
           };
 
-
     return (
         <div className="Pre-Start">
             <Slider text="Click to switch any meal"/>
             <div className="mealplan">
-
                 <div className="breakfast-top">
-                    <h1>Breakfast</h1>
-                    <button onClick = {sendApiRequest}></button>
+                <i onClick = {sendApiRequest} className="fas fa-rotate-right fa-3x generate"></i>
+                <h1 className="label">Breakfast</h1>
                 </div>
-                
+                {!mealData ? (
+                            <div />
+                        ) : (
+                          <div>
+                          <ul className="food-display">
+                          {mealData.breakfast.Names.map((foodItem, index) => (
+                          <FoodItem
+                            key={index}
+                            name={foodItem}
+                            calories={mealData.breakfast.Calories[index]}
+                            protein={mealData.breakfast.Protein[index]}
+                            carbs={mealData.breakfast.Carbs[index]}
+                            fat={mealData.breakfast.Fat[index]}
+                            serving={mealData.breakfast.Serving[index]}
+                          />
+                          ))}
+                          </ul>
+                          <div className="breakfast-top">
+                          <h1>Lunch</h1>
+                          </div>
+                          <ul className="food-display">
+                          {mealData.lunch.Names.map((foodItem, index) => (
+                          <FoodItem
+                            key={index}
+                            name={foodItem}
+                            calories={mealData.lunch.Calories[index]}
+                            protein={mealData.lunch.Protein[index]}
+                            carbs={mealData.lunch.Carbs[index]}
+                            fat={mealData.lunch.Fat[index]}
+                            serving={mealData.lunch.Serving[index]}
+                          />
+                          ))}
+                          </ul>
+                          <div className="breakfast-top">
+                          <h1>Dinner</h1>
+                          </div>
+                          <ul className="food-display">
+                          {mealData.dinner.Names.map((foodItem, index) => (
+                          <FoodItem
+                            key={index}
+                            name={foodItem}
+                            calories={mealData.dinner.Calories[index]}
+                            protein={mealData.dinner.Protein[index]}
+                            carbs={mealData.dinner.Carbs[index]}
+                            fat={mealData.dinner.Fat[index]}
+                            serving={mealData.dinner.Serving[index]}
+                          />
+                          ))}
+                          </ul>
+                        </div>
+                        )}
             </div>
         </div>
     )
 }
+
+const FoodItem = ({ name, calories, protein, carbs, fat, serving }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <li onClick={() => setIsExpanded(!isExpanded)}>
+      <button className="food-display">{name}</button>
+      {isExpanded && (
+        <div>
+          <p>Calories: {calories}g</p>
+          <p>Protein: {protein}g</p>
+          <p>Carbs: {carbs}g</p>
+          <p>Fat: {fat}g</p>
+          <p>Serving Size: {serving}</p>
+        </div>
+      )}
+    </li>
+  );
+};
